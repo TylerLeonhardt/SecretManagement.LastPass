4af59b23271f842087717359b551b468c87c4229
Tyler Leonhardt <tylerl0706@gmail.com>
2020-09-21 10:18:06 -0700

    add publish

--- build.ps1
+++ build.ps1
@@ -2,7 +2,11 @@
 param (
     [Parameter()]
     [switch]
-    $Package
+    $Package,
+
+    [Parameter()]
+    [switch]
+    $Publish
 )
 
 Push-Location $PSScriptRoot
@@ -19,6 +23,16 @@ if ($Package) {
     ) | ForEach-Object {
         Copy-Item -Path $_ -Destination (Join-Path $outDir $_) -Force -Recurse
     }
+}
+
+if ($Publish) {
+    Write-Host -ForegroundColor Green "Publishing module... here are the details:"
+    $moduleData = Import-Module -Force ./out/SecretManagement.LastPass -PassThru
+    Write-Host "Version: $($moduleData.Version)"
+    Write-Host "Prerelease: $($moduleData.PrivateData.PSData.Prerelease)"
+    Write-Host -ForegroundColor Green "Here we go..."
+
+    Publish-Module -Path ./out/SecretManagement.LastPass -NuGetApiKey (Get-Secret -Name PSGalleryApiKey -AsPlainText)
 }
 
 Pop-Location
